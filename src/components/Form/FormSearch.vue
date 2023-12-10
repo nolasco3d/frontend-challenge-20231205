67cc102ffe774175f71e0d1d9feb67ede85d187d
andre.castro <andre.castro@defensoria.mg.def.br>
2023-12-09 21:13:18 -0300

    Componente form para buscar hoteis

---
+++ src/components/Form/FormSearch.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-card flat>
+    <q-card-section>
+      <div class="text-h5 text-grey-8">Reservar Hotel</div>
+    </q-card-section>
+
+    <q-separator />
+
+    <q-card-section>
+      <div class="row q-col-gutter-lg">
+        <!-- Input -->
+        <div class="col-12">
+          <slot />
+        </div>
+
+        <!-- Button -->
+        <div class="col-12 flex justify-end">
+          <slot name="action">
+            <q-btn class="q-px-lg" unelevated rounded color="primary"
+              label="Buscar" @click="$emit('action')" />
+          </slot>
+        </div>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss"></style>
